c0446315faa660748e2412f60d286565d610c800
bjrn <bjoern.mathis@cispa.saarland>
2020-03-16 14:48:29 +0100

    added mutation of malloc as first example

--- modules/mutator/sources/mutator.cpp
+++ modules/mutator/sources/mutator.cpp
@@ -51,6 +51,10 @@ public:
 
     }
 
+    /**
+     * Instrument all functions given as parameter.
+     * @param functions
+     */
     void instrumentFunctions(std::vector<Function*> functions)
     {
         for (auto F: functions)
@@ -62,8 +66,127 @@ public:
                       << std::endl;
             builderMutex.unlock();
 
-//            instrumentFunction(*F);
+            mutateFunction(*F);
         }
+    }
+
+    /**
+     * Mutate the given function call if a mutation pattern exists for the function.
+     * @param builder the builder to add instruction in front of the call
+     * @param nextInstructionBuilder the builder to add instructions after the call
+     * @param instr the instruction to mutate (i.e. the function call)
+     * @param funNameString the name of the function that is called
+     * @return
+     */
+    bool mutateFunctionCall(IRBuilder<>* builder, IRBuilder<>* nextInstructionBuilder, Instruction* instr, const std::string& funNameString)
+    {
+        if (funNameString.find("malloc") != std::string::npos) {
+            // substract 1 and give the new value to malloc
+            Value* lhs;
+            if (CallInst* callinst = dyn_cast<CallInst>(instr))
+            {
+                lhs = callinst->getArgOperand(0); // TODO a pattern could describe which argument to mutate
+                builderMutex.lock();
+                auto newVal = builder->CreateAdd(lhs, builder->getInt64(-1));
+                builderMutex.unlock();
+                callinst->setOperand(0, newVal);
+            }
+            std::cout << "Found malloc!\n";
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * Instrument the given instruction with the given builders.
+     * @param instr
+     * @param builder
+     * @param nextInstructionBuilder
+     */
+    void mutateInstr(Instruction* instr, IRBuilder<>* builder, IRBuilder<>* nextInstructionBuilder)
+    {
+        // Call instructions are handled differently
+        if (CallInst* callinst = dyn_cast<CallInst>(instr))
+        {
+            Function* fun = callinst->getCalledFunction();
+            if (fun != nullptr && fun->isIntrinsic() && !dyn_cast<MemCpyInst>(callinst) && !dyn_cast<MemMoveInst>(callinst)
+                && !dyn_cast<VAStartInst>(callinst) && !dyn_cast<VAArgInst>(callinst) && !dyn_cast<VACopyInst>(callinst)
+                && !dyn_cast<VAEndInst>(callinst))
+            {
+                // skip llvm intrinsic functions other than llvm.memcpy and llvm memmove
+                return;
+            }
+
+            std::string calledFunctionName;
+            if (fun != nullptr)
+            {
+                calledFunctionName = fun->getName().str();
+            }
+            else
+            {
+                // called function might be unknown when using casts or indirect calls (through pointer)
+
+                // try harder to find the function name by removing casts and aliases
+                calledFunctionName = callinst->getCalledValue()->stripPointerCasts()->getName().str();
+                if (calledFunctionName.empty())
+                {
+                    // as a last resort use 'null' for the name
+                    // TODO (michael.mera) Really? It seems to me that 'null' is a misleading name, rather use 'unknown' or something similar.
+                    calledFunctionName = "null";
+                }
+            }
+
+            if (mutateFunctionCall(builder, nextInstructionBuilder, instr, calledFunctionName))
+            {
+                return;
+            }
+        }
+    }
+
+
+    /**
+     * Instrument one function, i.e. run over all isntructions in that function and instrument them.
+     * @param F the given function
+     * @return true on successful instrumentation
+     */
+    bool mutateFunction(Function& F)
+    {
+        std::vector<Instruction*> toInstrument;
+        for (BasicBlock& bb : F)
+        {
+            auto first_insertion_point = bb.getFirstInsertionPt();
+//
+//            IRBuilder<> builder(&bb, first_insertion_point);
+//
+//            std::vector<Value*> iHeaderArgs;
+//            createStaticArgList(&builder, iHeaderArgs, &*first_insertion_point);
+//            builderMutex.lock();
+//            iHeaderArgs[0] = builder.getInt64(additionalOperators.at("bbenter")->opcode);
+//            builder.CreateCall(tracer("instructionHeader"), iHeaderArgs);
+//            std::vector<Value*> bbEnterArgs;
+//            bbEnterArgs.push_back(builder.getInt64(bbIDCounter++));
+//            builder.CreateCall(tracer("enterBasicBlock"), bbEnterArgs);
+//            builder.CreateCall(tracer("instructionEnd"), {});
+//            builderMutex.unlock();
+
+            for (BasicBlock::iterator itr_bb = first_insertion_point; itr_bb != bb.end(); ++itr_bb)
+            {
+                toInstrument.push_back(&*itr_bb);
+            }
+        }
+
+        for (Instruction* instr : toInstrument)
+        {
+            BasicBlock::iterator itr_bb(instr);
+            builderMutex.lock();
+            IRBuilder<> builder(instr->getParent(), itr_bb);
+            IRBuilder<> nextInstructionBuilder(instr->getParent(), std::next(itr_bb, 1));
+            builderMutex.unlock();
+            mutateInstr(instr, &builder, &nextInstructionBuilder);
+        }
+
+        return true;
     }
 };
 
