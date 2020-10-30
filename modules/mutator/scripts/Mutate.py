ba69bff9535996600224667de9e33c5dc9184256
bjrn <bjoern.mathis@cispa.saarland>
2020-10-30 11:07:13 +0100

    improved handling of output files for mutation scripts

--- modules/mutator/scripts/Mutate.py
+++ modules/mutator/scripts/Mutate.py
@@ -5,6 +5,7 @@ import shutil
 import json
 from multiprocessing.dummy import Pool as ThreadPool
 from multiprocessing import cpu_count
+import argparse
 
 llvm_bindir = "@LLVM_BINDIR@"
 clang = f"{llvm_bindir}/clang"
@@ -16,20 +17,20 @@ progsource = None
 uname = os.uname()
 
 
-def main():
+def main(prog: str):
     # "${CLANG}" -g -S -D_FORTIFY_SOURCE=0 "${SYSROOT}" -emit-llvm -include "${INCDIR}/traceinstr/wrapper_libc.h" -o "${PROG_SOURCE}.uninstrumented.bc" -x c "${PROG_SOURCE}"
     # Macos catalina and newer need sysroot to be defined when compiling
     # TODO also check for actual version, not only if macos or not
     # "${LLVM}/opt" -S -instnamer -reg2mem -load "${TRACEPLUGIN}" -traceplugin -exclude_functions "${EXCLUDED_FUNCTIONS}" -disable-verify "${PROG_SOURCE}.uninstrumented.bc" -o  "${PROG_SOURCE}.opt_debug.bc"
 
     counter = 0
-    basepath = os.path.dirname(progsource)
+    basepath = os.path.dirname(prog)
     mutations_folder = os.path.join(basepath, "mutations")
-    progname = os.path.basename(progsource)
+    progname = os.path.basename(prog)
     print(f"[INFO] Folder to put mutations into: {mutations_folder}")
     shutil.rmtree(mutations_folder, ignore_errors=True)
     os.makedirs(mutations_folder)
-    with open(f"{progsource}.mutationlocations") as mutations:
+    with open(f"{prog}.mutationlocations") as mutations:
         mutation_list = []
         mutation_jsondata = json.load(mutations)
         for mutation in mutation_jsondata:
@@ -55,16 +56,35 @@ def mutate_file(information):
     with open(f"{progsource}.ll") as progsource_file:
         subprocess.call([opt, "-S", "-load", mutatorplugin, "-mutatorplugin", "-mutation_pattern", mutation, "-disable-verify", "-o", f"{mutations_folder}/{progname}.{counter}.mut.ll"], stdin=progsource_file)
 
-    if uname.sysname == "Darwin":
-        subprocess.call([clang, "-fno-inline", "-O3", "-isysroot", f"{sysroot}", "-o", f"{mutations_folder}/{progname}.{counter}.mut", f"{mutations_folder}/{progname}.{counter}.mut.ll", "-lm", "-lz", "-ldl"])
-    else:
-        subprocess.call([clang, "-fno-inline", "-O3", "-o", f"{mutations_folder}/{progname}.{counter}.mut", f"{mutations_folder}/{progname}.{counter}.mut.ll", "-lm", "-lz", "-ldl"])
+    if args.bitcode:
+        if uname.sysname == "Darwin":
+            subprocess.call([clang, "-emit-llvm", "-fno-inline", "-O3", "-isysroot", f"{sysroot}", "-o", f"{mutations_folder}/{progname}.{counter}.mut.bc", "-c", f"{mutations_folder}/{progname}.{counter}.mut.ll"])
+        else:
+            subprocess.call([clang, "-emit-llvm", "-fno-inline", "-O3", "-o", f"{mutations_folder}/{progname}.{counter}.mut.bc", "-c", f"{mutations_folder}/{progname}.{counter}.mut.ll"])
+
+    if args.binary:
+        if uname.sysname == "Darwin":
+            subprocess.call([clang, "-fno-inline", "-O3", "-isysroot", f"{sysroot}", "-o", f"{mutations_folder}/{progname}.{counter}.mut", f"{mutations_folder}/{progname}.{counter}.mut.ll", "-lm", "-lz", "-ldl"])
+        else:
+            subprocess.call([clang, "-fno-inline", "-O3", "-o", f"{mutations_folder}/{progname}.{counter}.mut", f"{mutations_folder}/{progname}.{counter}.mut.ll", "-lm", "-lz", "-ldl"])
+
+    if not args.bitcode_human_readable:
+        os.remove(f"{mutations_folder}/{progname}.{counter}.mut.ll")
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        progsource = sys.argv[1]
-        sysroot = "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/"
-        main()
-    else:
-        raise FileExistsError("Input C file is missing.")
+    parser = argparse.ArgumentParser(description="Mutator Script")
+    parser.add_argument('-bc', "--bitcode", action='store_true', help="Keeps the mutated bitcode files.")
+    parser.add_argument('-ll', "--bitcode_human_readable", action='store_true', help="Keeps the mutated bitcode files as human readable files.")
+    parser.add_argument('-bn', "--binary", action='store_true', help="Creates mutated runnable binaries.")
+    parser.add_argument('-p', "--program", default="", type=str, required=True,
+                        help="Path to the source file that will be mutated. Use at least one of the arguments [-bc, -ll, -bn] to get "
+                             "resulting files.")
+    args = parser.parse_args(sys.argv[1:])
+
+    if not (args.bitcode or args.bitcode_human_readable or args.binary):
+        raise ValueError("Use at least one of the arguments [-bc, -ll, -bn] to get resulting files.")
+
+    progsource = args.program
+    sysroot = "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/"
+    main(progsource)
