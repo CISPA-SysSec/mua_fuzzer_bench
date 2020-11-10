426c4a6a6cb6ef3b0dc34d0718ac2afde7717b2d
bjrn <bjoern.mathis@cispa.saarland>
2020-11-10 12:46:01 +0100

    improved deletion script

--- clean_mutations_in_samples.sh
+++ clean_mutations_in_samples.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-rm -rf samples/**/mutations
-rm -f samples/**/*.mutationlocations samples/**/*.ll samples/**/**/*.mutationlocations samples/**/**/*.ll
+find samples -name mutations -type d -print
+find samples -name '*.mutationlocations' -print
+find samples -name '*.ll' -print
+find samples -name '*.bc' -print
+find samples -name '*.o' -print
+
+find samples -name mutations -type d -exec rm -rf {} +
+find samples -name '*.mutationlocations' -delete
+find samples -name '*.ll' -delete
+find samples -name '*.bc' -delete
+find samples -name '*.o' -delete
