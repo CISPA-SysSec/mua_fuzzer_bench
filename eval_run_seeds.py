d4e4a1f70fa1392ce703508c8db97418c69f12b8
bjrn <bjoern.mathis@cispa.saarland>
2021-02-10 18:17:46 +0100

    fixes for missing triggers in mutation files compared to reference files and evaluation scripts to test those missing triggers

---
+++ eval_run_seeds.py
@@ -0,0 +1,15 @@
+import json
+
+found_false = set()
+
+with open("found_mutations.csv", "r") as found:
+    with open("fuzz_target.ll.mutationlocations", "r") as location_file:
+        for line in found.readlines():
+            if "False" in line:
+                found_false.add(int(line.split(",")[0]))
+        locations = json.load(location_file)
+        for location in locations:
+            if location["type"] == 14 and int(location["UID"]) in found_false:
+                found_false.remove(int(location["UID"]))
+
+print(found_false)
