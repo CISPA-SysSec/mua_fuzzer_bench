92263133faa7105669962b07597f2775c11f0d67
Abhilash Gupta <s8ahgupt@stud.uni-saarland.de>
2021-02-15 15:41:46 +0100

    Updated signed and unsigned less_than_equal_to comparisions to cover pointer comparisions. mutation_doc.json accordingly updated.

--- modules/utilities/src/main/cpp/iCmpInst_patterns.cpp
+++ modules/utilities/src/main/cpp/iCmpInst_patterns.cpp
@@ -47,8 +47,17 @@ bool SignedLessThanEqualToPattern::mutate(
             auto segref = *seglist;
             addMutationFoundSignal(builder, M, segref["UID"]);
             rhs = icmpinst->getOperand(1);
-            auto newVal = builder->CreateAdd(rhs, builder->getIntN(rhs->getType()->getIntegerBitWidth(), 1));
-            newVal = builder->CreateMul(newVal, builder->getIntN(rhs->getType()->getIntegerBitWidth(), 2));
+            Value *newVal;
+            if (rhs->getType()->isPointerTy()){
+                LLVMContext &llvmContext = M.getContext();
+                auto int_type = IntegerType::get (llvmContext, 32);
+                Value* indexList = ConstantInt::get(int_type, 8);
+                newVal = builder->CreateGEP(rhs, indexList);
+            }
+            else if (rhs->getType()->isIntegerTy()){
+                newVal = builder->CreateAdd(rhs, builder->getIntN(rhs->getType()->getIntegerBitWidth(), 1));
+                newVal = builder->CreateMul(newVal, builder->getIntN(rhs->getType()->getIntegerBitWidth(), 2));
+            }
             icmpinst->setOperand(1, newVal);
             builderMutex.unlock();
             return true;
@@ -155,8 +164,17 @@ bool UnsignedLessThanEqualToPattern::mutate(
             auto segref = *seglist;
             addMutationFoundSignal(builder, M, segref["UID"]);
             rhs = icmpinst->getOperand(1);
-            auto newVal = builder->CreateAdd(rhs, builder->getIntN(rhs->getType()->getIntegerBitWidth(), 1));
-            newVal = builder->CreateMul(newVal, builder->getIntN(rhs->getType()->getIntegerBitWidth(), 2));
+            Value *newVal;
+            if (rhs->getType()->isPointerTy()){
+                LLVMContext &llvmContext = M.getContext();
+                auto int_type = IntegerType::get (llvmContext, 32);
+                Value* indexList = ConstantInt::get(int_type, 8);
+                newVal = builder->CreateGEP(rhs, indexList);
+            }
+            else if (rhs->getType()->isIntegerTy()){
+                newVal = builder->CreateAdd(rhs, builder->getIntN(rhs->getType()->getIntegerBitWidth(), 1));
+                newVal = builder->CreateMul(newVal, builder->getIntN(rhs->getType()->getIntegerBitWidth(), 2));
+            }
             icmpinst->setOperand(1, newVal);
             builderMutex.unlock();
             return true;
