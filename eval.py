06e69ced88192c27fd2ec1a753efde79d2664d8b
Philipp Goerz <dev@philipp-goerz.com>
2021-05-26 11:04:04 +0200

    better "randomizing" of order mutations

--- eval.py
+++ eval.py
@@ -1324,13 +1324,40 @@ def get_all_mutations(stats, mutator, progs):
 
     return all_mutations
 
+def sequence_mutations(all_mutations):
+    """
+    Randomize order mutations in a way to get the most diverse mutations first.
+    Diverse as in the mutations are from different progs and different types.
+    """
+    random.shuffle(all_mutations)
+    grouped_mutations = defaultdict(list)
+    for mut in all_mutations:
+        prog = mut[1]
+        mutation_id = mut[0]
+        mutation_type = mut[4][int(mutation_id)]['type']
+        grouped_mutations[(prog, mutation_type)].append(mut)
+
+    sequenced_mutations = []
+
+    while len(grouped_mutations) > 0:
+        empty_lists = []
+        for key, mut_list in grouped_mutations.items():
+            sequenced_mutations.append(mut_list.pop())
+            if len(mut_list) == 0:
+                empty_lists.append(key)
+        for empty in empty_lists:
+            del grouped_mutations[empty]
+
+    return sequenced_mutations
+
 # Generator that first collects all possible runs and adds them to stats.
 # Then yields all information needed to start a eval run
 def get_all_runs(stats, fuzzers, progs):
     with start_mutation_container(None) as mutator:
         all_mutations = get_all_mutations(stats, mutator, progs)
 
-        random.shuffle(all_mutations)
+        all_mutations = sequence_mutations(all_mutations)
+
         all_runs = []
 
         # Go through the mutations
