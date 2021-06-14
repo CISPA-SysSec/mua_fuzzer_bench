1a2c74cb71f9046ddbbbc634d055eed01ffee485
Philipp Goerz <dev@philipp-goerz.com>
2021-06-14 14:19:01 +0200

    reimplement filter mutations

--- eval.py
+++ eval.py
@@ -1235,18 +1235,6 @@ def build_detector_binary(container, prog_info, detector_path, workdir):
             ]
     )
 
-    # compile_args = build_compile_args(prog_info['bc_compile_args'] + prog_info['bin_compile_args'], workdir)
-
-    # Assumes the prog has already been instrumented.
-    #  run_exec_in_container(container, True, [
-    #          "/usr/bin/clang++-11",
-    #          "-v",
-    #          "-o", str(detector_path),
-    #          *shlex.split(compile_args),
-    #          str(Path(workdir)/"tmp/lib/libdynamiclibrary.so"),
-    #          str(prog_info['orig_bc'])
-    #  ] )
-
 
 def get_all_mutations(stats, mutator, progs):
     all_mutations = []
@@ -1272,23 +1260,6 @@ def get_all_mutations(stats, mutator, progs):
 
         instrument_prog(mutator, prog_info)
 
-        if FILTER_MUTATIONS and DETECT_MUTATIONS:
-            detector_bin = Path(prog_info['orig_bc']).with_suffix(".ll.opt_mutate")
-            build_detector_binary(mutator, prog_info, detector_bin, IN_DOCKER_WORKDIR)
-            print("Filtering mutations, running all seed files.")
-            # Prepare the folder where the number of the generated seeds is put.
-            shutil.rmtree(mutation_list_dir, ignore_errors=True)
-            mutation_list_dir.mkdir(parents=True)
-            # Run the seeds through the detector binary.
-            detector_args_lines = ""
-            for seed in list(seeds.glob("*")):
-                detector_args = prog_info['args']
-                detector_args = detector_args.replace("<WORK>/", "").replace("@@", str(seed))
-                detector_args_lines += detector_args
-                detector_args_lines += "\n"
-            run_exec_in_container(mutator, False,
-                    ["./iterate_seeds.sh", mutation_list_dir, detector_bin, detector_args_lines])
-
         # get additional info on mutations
         mut_data_path = list(Path(HOST_TMP_PATH/prog_info['path'])
                                 .glob('**/*.ll.mutationlocations'))
@@ -1297,13 +1268,9 @@ def get_all_mutations(stats, mutator, progs):
         with open(mut_data_path, 'rt') as f:
             mutation_data = json.load(f)
 
-        # Get all mutations that are possible with that program, they are identified by the file names
-        # in the mutation_list_dir
-        if FILTER_MUTATIONS:
-            print("Filtering mutations, checking the found mutations.")
-            mutations = list((p.name, prog, prog_info, seeds, mutation_data) for p in mutation_list_dir.glob("*"))
-        else:
-            mutations = list((str(p['UID']), prog, prog_info, seeds, mutation_data) for p in mutation_data)
+        # Get all mutations that are possible with that program
+        mutations = list((str(p['UID']), prog, prog_info, seeds, mutation_data) for p in mutation_data)
+
         print(f"Found {len(mutations)} mutations for {prog}")
 
         for mut in mutations:
@@ -1524,6 +1491,13 @@ def handle_mutation_result(stats, prepared_runs, active_mutants, task_future, da
         clean_up_mut_base_dir(mut_data)
         return
 
+    # Check if we just want to do fuzzer runs for mutations that are covered
+    if FILTER_MUTATIONS:
+        # If the covered file was not seen, do not start any fuzzer runs
+        if not task_result['covered_file_seen']:
+            clean_up_mut_base_dir(mut_data)
+            return
+
     # Otherwise add all possible runs to prepared runs.
     for fr in fuzzer_runs:
         prepared_runs.put_nowait(fr)
@@ -1665,6 +1639,13 @@ def get_git_status():
 
 def build_subject_docker_images(progs):
     # build the subject docker images
+    known_programs = list(PROGRAMS.keys())
+
+    for prog in progs:
+        if prog not in known_programs:
+            print(f"Unknown program: {prog}, known programs are: {' '.join(known_programs)}")
+            sys.exit(1)
+
     for name in set(PROGRAMS[prog]['name'] for prog in progs):
         tag = subject_container_tag(name)
         print(f"Building docker image for {tag} ({name})")
