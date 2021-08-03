953d52a11363fec24ec71d7bdf15c0304e8c38eb
bjrn <bjoern.mathis@cispa.saarland>
2021-08-03 18:49:00 +0200

    some cleanups and fix of *printf functions badref after mutation

--- samples/simple_malloc/malloc.c
+++ samples/simple_malloc/malloc.c
@@ -6,14 +6,20 @@ int main(int argc, char** argv) {
     fgets(inp, 3, stdin);
     int size = atoi(inp);
     char* buffer = malloc(sizeof(char) * size);
+    char* buffer2 = malloc(sizeof(char) * size);
     for (int i = 0; i <= size - 1; i++) {
         printf("%d\n", i);
         buffer[i] = 'a';
     }
     buffer[size] = 65;
-    printf("%c\n", buffer[size]);
+    buffer2[size] = 66;
+    int result = printf("%c\n", buffer[size]);
+    printf("%d\n", result);
     buffer[size-1] = 0;
-    printf("%s\n", buffer);
+    result = sprintf(buffer2, "%s\n", buffer);
+    printf("%d\n", result);
+    result = snprintf(buffer2, size, "%s\n", buffer);
+    printf("%d\n", result);
     buffer[size-1] = 65;
     buffer[size] = 0;
     printf("%s\n", buffer);
