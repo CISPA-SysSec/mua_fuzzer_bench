3d583539bd8c38c159ab6c6b6d931e819c464874
Philipp Goerz <dev@philipp-goerz.com>
2021-09-15 11:22:29 +0200

    update download script

--- download_and_merge.sh
+++ download_and_merge.sh
@@ -2,6 +2,21 @@
 
 set -Eeuo pipefail
 
+COMBINED_DB_NAME="stats_all"
+
+if [ "$#" == 0 ]; then
+    echo "Specify target folder."
+    exit 1
+fi
+
+TARGET="$1"
+RESULT_DIR="data/$TARGET"
+CUR_RESULT_DIR="$RESULT_DIR/$(date '+%Y_%m_%d_%H:%M')"
+
+echo "Writing data to $RESULT_DIR"
+
+shift
+
 if [ "$#" == 0 ]; then
     echo "Please specify the ssh remotes to load databases from, at least one."
     exit 1
@@ -11,25 +26,33 @@ remotes=( "$@" )
 
 echo "Downloading dbs from: ${remotes[*]}"
 
-rm -rf /tmp/mutator_dbs/
-mkdir -p /tmp/mutator_dbs/
-
-db_paths=()
+mkdir -p "$CUR_RESULT_DIR"
 
 for remote in "${remotes[@]}"; do
-    db_path="/tmp/mutator_dbs/stats_$remote.db"
+    db_path="$(pwd)/$CUR_RESULT_DIR/stats_$remote.db"
     (ssh "$remote" 'cp /dev/shm/mutator/stats.db /dev/shm/mutator/stats_copy.db' && \
         scp "$remote":/dev/shm/mutator/stats_copy.db "$db_path") &
-    db_paths+=("$db_path")
+    active_db="$RESULT_DIR/$remote"
+    touch "$active_db" && rm "$active_db"
+    ln -s "$db_path" "$active_db"
 done
 
 wait
 
 echo "Merging dbs:"
 
-./eval.py merge ~/stats_all.db "${db_paths[@]}"
+db_paths=()
+for db in "$RESULT_DIR"/*; do
+    if [[ -f "$db" && ! $(basename "$db") =~ ^$COMBINED_DB_NAME ]]; then
+        echo "will merge db: $db"
+        db_paths+=("$db")
+    fi
+done
+
+COMBINED_DB="$RESULT_DIR/$COMBINED_DB_NAME.db"
+
+./eval.py merge "$COMBINED_DB" "${db_paths[@]}"
 
 echo "Plotting merged db:"
 
-./eval.py plot ~/stats_all.db
-
+./eval.py plot "$COMBINED_DB"
