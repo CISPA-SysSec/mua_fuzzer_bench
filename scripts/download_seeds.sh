829519a12260d5e1744da7049d42bd3e5434872e
Philipp Goerz <dev@philipp-goerz.com>
2021-11-24 14:48:51 +0100

    fix download script

--- scripts/download_seeds.sh
+++ scripts/download_seeds.sh
@@ -3,6 +3,5 @@ set -euo pipefail
 
 mkdir tmp/seeds
 curl https://ruhr-uni-bochum.sciebo.de/s/7sfECFTqcv3dIMe/download > /tmp/seeds.zip
-unzip -d tmp/seeds /tmp/seeds.zip
-(cd /tmp && unzip -q seeds.zip)
+unzip -q -d tmp/seeds /tmp/seeds.zip
 rm /tmp/seeds.zip
