887689885429288c9d8bce8e64aefe0c35ab43d4
Philipp Goerz <dev@philipp-goerz.com>
2021-11-28 11:54:41 +0100

    add subject mjs

--- subjects/Dockerfile.mjs
+++ subjects/Dockerfile.mjs
@@ -2,13 +2,17 @@ FROM mutator_mutator
 
 ################################################################################
 # mjs
+
+# clone
 WORKDIR /home/mutator/samples
 RUN git clone https://github.com/cesanta/mjs.git
-WORKDIR /home/mutator/samples/mjs/mjs
-RUN git checkout 2.19.1
-COPY samples/mjs_harness /home/mutator/samples/mjs_harness
-RUN make mjs.c
-RUN gclang -g -O2 -DMJS_MAIN -ldl -o mjs mjs.c
-RUN get-bc mjs
-WORKDIR /home/mutator
+WORKDIR /home/mutator/samples/mjs
+RUN git checkout b1b6eac6b1e5b830a5cb14f8f4dc690ef3162551
 
+# build
+COPY samples/mjs_harness /home/mutator/samples/mjs_harness
+RUN CFLAGS="" CXXFLAGS="" \
+    SRC="/home/mutator/samples/mjs" \
+    WORK="/home/mutator/samples/mjs/work" \
+    OUT="/home/mutator/samples/mjs/out" \
+    ../mjs_harness/build.sh
