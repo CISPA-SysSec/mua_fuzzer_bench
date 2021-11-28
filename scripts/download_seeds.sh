58ee996feef90c64a947d8994d127fc529b6ad1c
Philipp Goerz <dev@philipp-goerz.com>
2021-11-28 11:54:54 +0100

    update seeds

--- scripts/download_seeds.sh
+++ scripts/download_seeds.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 mkdir tmp/seeds
-curl https://ruhr-uni-bochum.sciebo.de/s/7sfECFTqcv3dIMe/download > /tmp/seeds.zip
+curl https://ruhr-uni-bochum.sciebo.de/s/66e1uvRoYL3FSE1/download > /tmp/seeds.zip
 unzip -q -d tmp/seeds /tmp/seeds.zip
 rm /tmp/seeds.zip
