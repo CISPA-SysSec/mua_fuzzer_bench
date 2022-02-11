80bd5cbfbd9bdf0df3c6666b3862a5ea13b958c2
bjrn <bjoern.mathis@cispa.saarland>
2022-02-11 20:36:51 +0100

    improved test scripts to see if generated mutationlocations are faulty or miss information

--- cov/deduplicator.py
+++ cov/deduplicator.py
@@ -6,30 +6,38 @@ their number and generates a deduplicated locations file.
 import json
 import multiprocessing.pool
 import os
-import sys
 
-new_locs = dict()
 
 def find_duplicates(args):
     arg_value = args[0]
     locs = args[1]
-    result = [el_key for el_key, el_value in locs.items() if el_value == arg_value]
+    result = [el_key for el_key, el_value in locs[arg_value["funname"]].items() if el_value == arg_value]
     if len(result) > 1:
-        print(result)
+        # print(result)
         return result
 
+
 def main(mutationlocations_filepath: str):
+    new_locs = dict()
     with open(mutationlocations_filepath, "r") as ml_file:
         locations = json.load(ml_file)
+        to_check = list()
         for loc in locations:
-            new_locs[loc["UID"]] = loc
+            funloc = new_locs.setdefault(loc["funname"], dict())
+            funloc[loc["UID"]] = loc
             loc["UID"] = 0
-        to_check = [(value, new_locs) for key, value in new_locs.items()]
+            to_check.append((loc, new_locs))
         with multiprocessing.pool.Pool(os.cpu_count()) as pool:
-            duplicates = {tuple(el) for el in pool.imap(find_duplicates, to_check, chunksize=100) if el}
+            duplicates = {tuple(el) for el in pool.map(find_duplicates, to_check) if el}
         print(duplicates)
         print(len(duplicates))
 
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    # main(sys.argv[1])
+    main("convert_woff2ttf_fuzzer_new_entry.ll.mutationlocations")
+    counter = 1
+    while os.path.exists(f"woff2{counter}.mutationlocations"):
+        main(f"woff2{counter}.mutationlocations")
+        print(f"woff2{counter}.mutationlocations")
+        counter += 1
--- cov/findduplicate.py
+++ cov/findduplicate.py
@@ -1,45 +1,51 @@
 #!/usr/bin/env python3
 """
-Takes a mutationlocations file and checks how many non-unique locations exist, prints those with
-their number and generates a deduplicated locations file.
+Takes two mutationlocations files and checks if there is exactly one matching location for each mutation both files.
+I.e. for each location in one file there must be exactly one other location in the other file.
 """
 import json
 import multiprocessing.pool
 import os
 import sys
 
-new_locs1 = dict()
-new_locs2 = dict()
 
 def find_duplicates(args):
     arg_key = args[0]
     arg_value = args[1]
     locs = args[2]
-    result = [(arg_key, el_key) for el_key, el_value in locs.items() if el_value == arg_value]
+    result = [(arg_key, el_key) for el_key, el_value in locs[arg_value["funname"]].items() if el_value == arg_value]
+    # if len(result) != 1 or (len(result) == 1 and result[0][0] == result[0][1]):
     if len(result) != 1:
-        print(result)
+        # print(result)
         return result
     # else:
     #     print(result)
 
 
 def main(mutationlocations_filepath1: str, mutationlocations_filepath2: str):
+    new_locs1 = dict()
+    new_locs2 = dict()
     with open(mutationlocations_filepath1, "r") as ml1_file:
         with open(mutationlocations_filepath2, "r") as ml2_file:
             locations1 = json.load(ml1_file)
             locations2 = json.load(ml2_file)
             for loc in locations1:
-                new_locs1[loc["UID"]] = loc
+                funloc = new_locs1.setdefault(loc["funname"], dict())
+                funloc[loc["UID"]] = loc
                 loc["UID"] = 0
             for loc in locations2:
                 new_locs2[loc["UID"]] = loc
                 loc["UID"] = 0
-            to_check = [(key, value, new_locs2) for key, value in new_locs1.items()]
+            to_check = [(key, value, new_locs1) for key, value in new_locs2.items()]
             with multiprocessing.pool.Pool(os.cpu_count()) as pool:
-                duplicates = {tuple(el) for el in pool.imap(find_duplicates, to_check, chunksize=100) if el}
+                duplicates = {tuple(el) for el in pool.map(find_duplicates, to_check) if el}
             print(duplicates)
         print(len(duplicates))
 
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2])
+    counter = 1
+    while os.path.exists(f"woff2{counter}.mutationlocations"):
+        main(f"woff21.mutationlocations", f"woff2{counter}.mutationlocations")
+        print(f"woff2{counter}.mutationlocations")
+        counter += 1
