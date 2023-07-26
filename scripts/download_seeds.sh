cad02a91e310a500282502b2a25e62c91501dd2f
phi-go <github@philipp-goerz.com>
2023-07-26 17:13:16 +0200

    remove scripts

--- scripts/download_seeds.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-mkdir tmp/seeds
-curl https://ruhr-uni-bochum.sciebo.de/s/66e1uvRoYL3FSE1/download > /tmp/seeds.zip
-unzip -q -d tmp/seeds /tmp/seeds.zip
-rm /tmp/seeds.zip
