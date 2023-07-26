b3cc3815f9dce9371eb5d461bb5beb888c032327
Philipp Görz <github@philipp-goerz.com>
2023-07-26 17:16:26 +0200

    remove scripts (#5)

--- scripts/attach_eval_container.sh
+++
@@ -1,6 +0,0 @@
-
-NAME=$1
-echo $NAME
-
-sh start_eval_container.sh $NAME
-docker exec -it "${NAME}_container" /bin/bash
--- scripts/delete_eval_container.sh
+++
@@ -1,7 +0,0 @@
-
-NAME=$1
-echo $NAME
-
-docker stop "${NAME}_container"
-docker rm "${NAME}_container"
-docker image rm "eval_${NAME}"
